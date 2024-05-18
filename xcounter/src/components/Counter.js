e006a5340671466f4bf90d50e98e94f076c42b33
bijlwan07ankit <bijlwan07ankit@gmail.com>
2024-05-18 11:35:05 +0530

    correction  in app

--- xcounter/src/components/Counter.js
+++ xcounter/src/components/Counter.js
@@ -1,27 +1,22 @@
-import react from "react";
-import {useState} from 'react';
-import Styles from "../components/Counter.module.css"
+import React from 'react'
+import { useState } from 'react'
 
-export default function Counter(){
-    const [count,setCount]=useState(0);
-    const add=(()=>{
-        setCount((count)=>count+1)
-    })
-
-    const sub=(()=>{
-        setCount((count)=> count-1)
-})
-    return(
-        <div className={Styles.app}>
+const Counter = () => {
+  const [count,setCount] = useState(0)
+  const incrementBtn = () => {
+    setCount((prev) => prev + 1)
+  }
+  const decrementBtn = () => {
+    setCount((prev) => prev - 1)
+  }
+  return (
+    <div>
         <h1>Counter App</h1>
-        <p>Count:{count}</p>
-       
-       <button
-       onClick={add}
-       >Increment</button>
-        <button onClick={sub}
-        >Decrement</button>
+        <p>Count: {count}</p>
+        <button onClick={incrementBtn}>Increment</button>
+        <button onClick={decrementBtn}>Decrement</button>
     </div>
-
-    )
+  )
 }
+
+export default Counter
